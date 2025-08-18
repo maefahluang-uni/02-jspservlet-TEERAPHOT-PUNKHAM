a054fcfba06d409261c2f95bedf61dbdfc1a24c3
THIRAPHOT PUNKHAM <6731503014@lamduan.mfu.ac.th>
2025-08-18 09:05:46 +0000

    finish

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -2,6 +2,7 @@ package th.mfu;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,20 +10,47 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
+        
         
         //TODO: calculate bmi
+        double bmirun = weight / (height * height);
+        int bmi = (int)Math.round(bmirun);
+        
 
         //TODO: determine the built from BMI
-      
+        String builtType; 
+        if (bmi < 18.5) {
+            builtType = "Underweight";
+        } else if (bmi < 25) {
+            builtType = "Normal";
+        } else if (bmi < 30) {
+            builtType = "Overweight";
+        } else if (bmi < 35) {
+            builtType = "Obese";
+        } else {
+            builtType = "Extremely Obese";
+        }
+        
+        
         //TODO: add bmi and built to the request's attribute
+        request.setAttribute("bmi", bmi);
+        request.setAttribute("builtType", builtType);
+
 
         //TODO: forward to jsp
-           
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/bmi_result.jsp");
+        dispatcher.forward(request, response);
     }
-    
 }
+           
+    
+    
+
